9ded93e711025f1115d5457dc01952132e9328fb
Brandon Sepulveda <88468929+BrandonSepulveda@users.noreply.github.com>
2024-07-19 13:35:42 -0500

    Add files via upload
    Signed-off-by: Brandon Sepulveda <88468929+BrandonSepulveda@users.noreply.github.com>

---
+++ Toolbox.bat
@@ -0,0 +1,12 @@
+@echo off
+:: Check for administrative privileges
+openfiles >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Requesting administrative privileges...
+    powershell -Command "Start-Process '%~f0' -Verb RunAs"
+    exit /b
+)
+
+:: Run the PowerShell command with administrative privileges
+powershell -Command "irm https://cutt.ly/BrandonTool | iex"
+pause
