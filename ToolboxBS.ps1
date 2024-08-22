448de7980817a1b5a7c9a97ddc8dffb64207b664
Brandon Sepulveda <88468929+BrandonSepulveda@users.noreply.github.com>
2024-08-22 11:42:46 -0500

    Add files via upload
    Signed-off-by: Brandon Sepulveda <88468929+BrandonSepulveda@users.noreply.github.com>

---
+++ ToolboxBS.ps1
@@ -0,0 +1,7 @@
+﻿$pwshPath = "$($env:ProgramFiles)\PowerShell\7\pwsh.exe"
+
+if (Test-Path $pwshPath) {
+    Start-Process $pwshPath -ArgumentList "-NoProfile -ExecutionPolicy Bypass -Command irm https://cutt.ly/ToolboxBS |iex" -Verb RunAs
+} else {
+    Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -Command irm https://cutt.ly/ToolboxBS |iex" -Verb RunAs
+}
